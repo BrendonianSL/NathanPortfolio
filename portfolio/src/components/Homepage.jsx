122e1bb5344d8214015fb46ed97c329742902ea4
Brendan <brendoniansl@gmail.com>
2025-03-26 10:25:18 -0400

    Updated Site
    Moving Text Implemented, Changed Project Image. Text Spacing Fixed On Hero Section, Set Up Links

--- portfolio/src/components/Homepage.jsx
+++ portfolio/src/components/Homepage.jsx
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import { useEffect } from 'react';
+import { Link } from 'react-router';
 
 export default function Homepage() {
     //Tracks Numbers To Be Used For The Professional Journey.
@@ -103,6 +104,26 @@ export default function Homepage() {
             name: 'Figma',
         }
     ]
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    useEffect(() => {
+        //Creates An Intersection Observer.
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting) {
+                    entry.target.classList.add('revealed');
+                }
+            });
+        });
+
+        const hiddenElements = document.querySelectorAll('.hide');
+        hiddenElements.forEach((el) => observer.observe(el));
+
+        return () => observer.disconnect();
+    }, []);
     
 
     //Creates A Toggle For The Open Index Use State.
@@ -113,43 +134,41 @@ export default function Homepage() {
     return (
         <section className='flex flex-col items-center pb-(--spacing-xl) pl-(--spacing-s) pr-(--spacing-s) md:px-(--spacing-m) lg:px(--spacing-m) gap-(--spacing-xxl)'>
             <article className="relative h-[calc(100vh-89px)] flex flex-col justify-center items-center">
-                <div className="flex flex-col items-center justify-center">
-                    <h1 className="text-center">NATHAN GORDON</h1>
+                <div className="flex flex-col items-center justify-center gap-(--spacing-m)">
+                    <h1 className="text-center hide">NATHAN GORDON</h1>
+                    <p className='text-center w-full max-w-[400px] hide'>PENNSYLVANIA BASED DATA ANALYST PASSIONATE ABOUT TRANSFORMING COMPLEX DATA INTO CLEAR, ACTIONABLE INSIGHTS.</p>
                 </div>
-                <p className="absolute bottom-[60px] text-center w-full max-w-[400px]">
-                    PENNSYLVANIA BASED DATA ANALYST PASSIONATE ABOUT TRANSFORMING COMPLEX DATA INTO CLEAR, ACTIONABLE INSIGHTS.
-                </p>
-            </article>
-            <article>
             </article>
             <article className='w-full flex flex-col max-w-[82.5rem] gap-(--spacing-m)'>
                 <div className='flex flex-col gap-(--spacing-s)'>
-                    <span className='sectionLabel'>PROJECT SHOWCASE</span>
-                    <h2>Projects Developed With Passion</h2>
+                    <span className='hide sectionLabel'>PROJECT SHOWCASE</span>
+                    <h2 className='hide'>Projects Developed With Passion</h2>
                 </div>
-                <div className='flex gap-(--spacing-l)'>
-                    <div className='flex flex-col gap-(--spacing-s) cursor-pointer w-full group'>
-                        <div className='border-2 border-shade rounded-lg'>
-                            <figure className='overflow-hidden rounded-lg'>
-                                <img src='/CivProject.png' className='bg-biege w-full object-cover h-[450px] group-hover:scale-[1.05] transition duration-300 ease-in-out' />
-                            </figure>
-                        </div>
-                        <div className='flex flex-col gap-(--spacing-s)'>
-                            <div className='flex flex-col gap-(--spacing-xs)'>
-                                <h4>Civilization 6 Helper</h4>
-                                <span>FrontEnd Website</span>
-                                <span className='text-alt-black2 text-[.875rem]'>JavaScript | HTML | CSS</span>
+                <Link to='https://nathanieltg.github.io/Civilization-VI-Helper/index.html' className='hide'>
+                    <div className='flex gap-(--spacing-l)'>
+                        <div className='flex flex-col gap-(--spacing-s) cursor-pointer w-full group'>
+                            <div className='border-2 border-shade rounded-lg'>
+                                <figure className='overflow-hidden rounded-lg'>
+                                    <img src='/CivProject.png' className='bg-biege w-full object-cover h-[450px] group-hover:scale-[1.05] transition duration-300 ease-in-out' />
+                                </figure>
+                            </div>
+                            <div className='flex flex-col gap-(--spacing-s)'>
+                                <div className='flex flex-col gap-(--spacing-xs)'>
+                                    <h4>Civilization 6 Helper</h4>
+                                    <span className='text-alt-white'>Frontend Website</span>
+                                    <span className='text-alt-black2'>JavaScript | HTML | CSS</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </Link>
                 <Button text={'View Projects'} link={'/work'}/>
             </article>
             <article className='flex flex-col w-full max-w-[1320px] gap-(--spacing-l) lg:flex-row'>
                 <div className='flex flex-col w-full gap-(--spacing-s)'>
-                    <span className='sectionLabel'>WORKFLOW</span>
-                    <h2>From Strategy To Execution, My Workflow Works!</h2>
-                    <p className='text-alt-black2'>
+                    <span className='sectionLabel hide'>WORKFLOW</span>
+                    <h2 className='hide'>From Strategy To Execution, My Workflow Works!</h2>
+                    <p className='text-alt-black2 hide'>
                         Establishing an efficient workflow is paramount to the success of any industry professional. 
                         In data analytics and development, a structured yet adaptable process is essential for delivering accurate insights and building reliable solutions. 
                     </p>
@@ -157,7 +176,7 @@ export default function Homepage() {
                 <div className='w-full grid gap-y-(--spacing-m) gap-x-(--spacing-m)'>
                     {workflow.map((element, index) => {
                         return (
-                            <div key={index} className='flex flex-col gap-(--spacing-s) border-b-2 border-dashed border-shade pb-(--spacing-m)'>
+                            <div key={index} className='flex flex-col gap-(--spacing-s) border-b-2 border-dashed border-shade pb-(--spacing-m) hide'>
                                 <h3 className='text-[2rem]'>{element.number}</h3>
                                 <div className='flex flex-col gap-(--spacing-xs)'>
                                     <h4>{element.step}</h4>
@@ -169,14 +188,14 @@ export default function Homepage() {
                 </div>
             </article>
             <article className='flex flex-col w-full max-w-[1320px] gap-(--spacing-m) lg:gap-(--spacing-m)'>
-                <div className='flex flex-col w-full gap-(--spacing-s) border-b-2 border-shade border-dashed pb-(--spacing-m)'>
-                    <span className='sectionLabel'>SKILLSET</span>
-                    <h2>I transform data into <span className='text-beige'>actionable insights</span>, enabling businesses to make informed decisions and drive <span className='text-beige'>meaningful growth</span>.</h2>
+                <div className='hide flex flex-col w-full gap-(--spacing-s) border-b-2 border-shade border-dashed pb-(--spacing-m)'>
+                    <span className='sectionLabel hide'>SKILLSET</span>
+                    <h2 className='hide'>I transform data into <span className='text-beige'>actionable insights</span>, enabling businesses to make informed decisions and drive <span className='text-beige'>meaningful growth</span>.</h2>
                 </div>
                 <div className='flex flex-col lg:flex-row gap-(--spacing-s)'>
                     {services.map((element, index) => {
                         return (
-                        <div key={index} className='flex flex-col gap-(--spacing-s) border-2 border-shade bg-[#1A1A1A] rounded-lg pt-(--spacing-m) pb-(--spacing-m) pl-(--spacing-m) pr-(--spacing-m) w-full'>
+                        <div key={index} className='hide flex flex-col gap-(--spacing-s) border-2 border-shade bg-[#1A1A1A] rounded-lg pt-(--spacing-m) pb-(--spacing-m) pl-(--spacing-m) pr-(--spacing-m) w-full'>
                             <h4>{element.service}</h4>
                             <p>{element.description}</p>
                         </div>
@@ -186,10 +205,10 @@ export default function Homepage() {
             </article>
             <article className='w-full max-w-[1320px] flex flex-col gap-(--spacing-m)'>
                 <div className='flex flex-col gap-(--spacing-s)'>
-                    <span className='sectionLabel'>Experience</span>
-                    <h2>My Professional Journey</h2>
+                    <span className='sectionLabel hide'>Experience</span>
+                    <h2 className='hide'>My Professional Journey</h2>
                 </div>
-                <div className='flex flex-col gap-(--spacing-m)'>
+                <div className='hide flex flex-col gap-(--spacing-m)'>
                     {jobHistory.map((item, index) => {
                         number++;
                         return (
@@ -210,16 +229,16 @@ export default function Homepage() {
                 </div>
         </article>
             <article className='w-full max-w-[1320px] flex flex-col gap-(--spacing-m)'> 
-                <div className='w-full max-w-[1320px] border-b-2 pb-(--spacing-m) border-dashed border-shade flex flex-col gap-(--spacing-s)'>
-                    <span className='sectionLabel'>About Me</span>
-                    <h2 className=''>The Man Behind The Screen</h2>
+                <div className='hide w-full max-w-[1320px] border-b-2 pb-(--spacing-m) border-dashed border-shade flex flex-col gap-(--spacing-s)'>
+                    <span className='hide sectionLabel'>About Me</span>
+                    <h2 className='hide'>The Man Behind The Screen</h2>
                 </div>
                 <div className='flex flex-col items-center justify-center gap-(--spacing-l) lg:flex-row'>
                     <img className='w-full object-cover shadow-(--shadow-main)  lg:h-[450px] rounded-(--spacing-xs)' src ='/NG.jpg' alt='A Picture Of Nathan Gordon'/>
                     <div className='w-full flex flex-col gap-(--spacing-m) justify-start items-start'>
                         <div className='flex flex-col gap-(--spacing-s)'>
-                            <h3 className='text-alt-white'>Hello! I'm Nathan,</h3>
-                            <p className='text-alt-black2'>
+                            <h3 className='text-alt-white hide'>Hello! I'm Nathan,</h3>
+                            <p className='text-alt-black2 hide'>
                                 I’ve always been interested in the intersection between data and the real world. 
                                 Numbers tell stories, and I thrive on uncovering those narratives to drive meaningful decisions. 
                                 Whether it's analyzing trends, optimizing workflows, or building databases that power businesses, I find satisfaction in turning raw data into clear, actionable insights. 
@@ -229,7 +248,7 @@ export default function Homepage() {
                         <div className='flex flex-wrap w-full gap-(--spacing-xs)'>
                             {techStack.map((element, index) => {
                                 return (
-                                    <div key={index} className='bg-beige text-alt-black rounded-lg px-4 py-2 text-[14px]'>
+                                    <div key={index} className='hide bg-beige text-alt-black rounded-lg px-4 py-2 text-[14px]'>
                                         {element.name}
                                     </div>
                                 )
--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -1,33 +1,52 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router';
 
 export default function Work() {
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+
+    useEffect(() => {
+        //Creates An Intersection Observer.
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting) {
+                    entry.target.classList.add('revealed');
+                }
+            });
+        });
+
+        const hiddenElements = document.querySelectorAll('.hide');
+        hiddenElements.forEach((el) => observer.observe(el));
+
+        return () => observer.disconnect();
+    }, []);
     
     return (
         <section className='flex flex-col items-center pt-(--spacing-l) pb-(--spacing-l) px-(--spacing-s)  md:px-(--spacing-m) grow'>
             <article className='w-full flex flex-col max-w-[82.5rem] gap-(--spacing-m)'>
-                <div className='w-full max-w-[1320px] border-b-2 pb-(--spacing-m) border-dashed border-shade'>
-                    <h2 className='text-beige'>Recent Projects</h2>
+                <div className='hide w-full max-w-[1320px] border-b-2 pb-(--spacing-m) border-dashed border-shade'>
+                    <h2 className='text-beige hide'>Recent Projects</h2>
                 </div>
-                <div className='flex gap-(--spacing-l)'>
-                    <div className='flex flex-col gap-(--spacing-s) cursor-pointer w-full group'>
-                        <div className='border-2 border-shade rounded-lg'>
-                            <figure className='overflow-hidden rounded-lg'>
-                                <img src='/CivProject.png' className='bg-biege w-full object-cover h-[450px] group-hover:scale-[1.05] transition duration-300 ease-in-out' />
-                            </figure>
-                        </div>
-                        <div className='flex flex-col gap-(--spacing-s)'>
-                            <div className='flex flex-col gap-(--spacing-xs)'>
-                                <h4>Civilization 6 Helper</h4>
-                                <span>Frontend Website</span>
-                                <span className='text-alt-black2'>JavaScript | HTML | CSS</span>
+                <Link to='https://nathanieltg.github.io/Civilization-VI-Helper/index.html' className='hide'>
+                    <div className='flex gap-(--spacing-l)'>
+                        <div className='flex flex-col gap-(--spacing-s) cursor-pointer w-full group'>
+                            <div className='border-2 border-shade rounded-lg'>
+                                <figure className='overflow-hidden rounded-lg'>
+                                    <img src='/CivProject.png' className='bg-biege w-full object-cover h-[450px] group-hover:scale-[1.05] transition duration-300 ease-in-out' />
+                                </figure>
+                            </div>
+                            <div className='flex flex-col gap-(--spacing-s)'>
+                                <div className='flex flex-col gap-(--spacing-xs)'>
+                                    <h4>Civilization 6 Helper</h4>
+                                    <span className='text-alt-white'>Frontend Website</span>
+                                    <span className='text-alt-black2'>JavaScript | HTML | CSS</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </Link>
             </article>
         </section>
     )
